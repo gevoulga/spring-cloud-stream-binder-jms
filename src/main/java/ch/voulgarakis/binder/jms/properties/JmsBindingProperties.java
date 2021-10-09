43cb541df988fab5596ebc35de33bc8f41a2f6a8
gevoulga <mail@voulgarakis.ch>
2021-10-09 09:21:14 +0200

    fix binding properties

--- src/main/java/ch/voulgarakis/binder/jms/properties/JmsBindingProperties.java
+++ src/main/java/ch/voulgarakis/binder/jms/properties/JmsBindingProperties.java
@@ -7,6 +7,6 @@ import org.springframework.cloud.stream.binder.BinderSpecificPropertiesProvider;
 @Getter
 @Setter
 public class JmsBindingProperties implements BinderSpecificPropertiesProvider {
-    private JmsProducerProperties jmsProducerProperties = new JmsProducerProperties();
-    private JmsConsumerProperties jmsConsumerProperties = new JmsConsumerProperties();
+    private JmsProducerProperties producer = new JmsProducerProperties();
+    private JmsConsumerProperties consumer = new JmsConsumerProperties();
 }
